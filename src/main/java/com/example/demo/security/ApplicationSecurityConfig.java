3d0bbce2cebbba3fae848c46845fff1c91e4d290
janhavas <havas.jan@gmail.com>
2023-06-30 15:11:26 +0200

    Adding .dispatcherTypeMatchers to permit web root.

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,35 @@
 package com.example.demo.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static jakarta.servlet.DispatcherType.ERROR;
+import static jakarta.servlet.DispatcherType.FORWARD;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ApplicationSecurityConfig {
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeHttpRequests((authz) -> authz
+                    //Dispatches FORWARD and ERROR are permitted to allow Spring MVC to render views and Spring Boot to render errors
+                    .dispatcherTypeMatchers(FORWARD, ERROR).permitAll()
+                    .requestMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                    .anyRequest()
+                    .authenticated())
+
+    .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+}
 
 
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -23,4 +23,5 @@ public class StudentController {
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exists"));
     }
+
 }
